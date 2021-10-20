5d0f84c840592b5db90eea858d8af7cee483026c
Piotr Kukla <piotr.kukla19@gmail.com>
2021-10-20 10:55:23 +0200

    Add search notes functionality

--- src/components/Search/index.tsx
+++ src/components/Search/index.tsx
@@ -1,7 +1,23 @@
+import useNotes from 'components/NotesProvider';
+import { DebounceInput } from 'react-debounce-input';
 import { StyledInput } from './SearchElements';
 
 const Search: React.FC = () => {
-  return <StyledInput label="search" type="search" />;
+  const { setNotes } = useNotes()!;
+
+  const onChange = (
+    e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>,
+  ) => setNotes({ type: 'SEARCH', payload: e.target.value });
+
+  return (
+    <DebounceInput
+      onChange={onChange}
+      element={StyledInput}
+      label="search"
+      type="search"
+      debounceTimeout={150}
+    />
+  );
 };
 
 export default Search;
